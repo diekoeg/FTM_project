5447bf1b754b5f7f4e51e25a92780f17d6400384
Diego Estrada <diego_estrada14@hotmail.com>
2023-10-30 21:28:10 -0600

    Timeline placeholder para eventos creada

---
+++ src/components/TimelineEvents/index.jsx
@@ -0,0 +1,69 @@
+import { Button, Timeline } from 'flowbite-react';
+import { HiArrowNarrowRight, HiCalendar } from 'react-icons/hi';
+import React from 'react'
+
+function TimelineEvents() {
+
+  const placeholder = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+  return (
+    <Timeline>
+      <Timeline.Item>
+        <Timeline.Point icon={HiCalendar} />
+        <Timeline.Content>
+          <Timeline.Time>
+            February 2022
+          </Timeline.Time>
+          <Timeline.Title>
+            Titulo 1
+          </Timeline.Title>
+          <Timeline.Body>
+            <p>
+              {placeholder}
+            </p>
+          </Timeline.Body>
+          <Button color="gray">
+            <p>
+              Ver más
+            </p>
+            <HiArrowNarrowRight className="ml-2 h-3 w-3" />
+          </Button>
+        </Timeline.Content>
+      </Timeline.Item>
+      <Timeline.Item>
+        <Timeline.Point icon={HiCalendar} />
+        <Timeline.Content>
+          <Timeline.Time>
+            March 2022
+          </Timeline.Time>
+          <Timeline.Title>
+            Titulo 2
+          </Timeline.Title>
+          <Timeline.Body>
+            <p>
+              {placeholder}
+            </p>
+          </Timeline.Body>
+        </Timeline.Content>
+      </Timeline.Item>
+      <Timeline.Item>
+        <Timeline.Point icon={HiCalendar} />
+        <Timeline.Content>
+          <Timeline.Time>
+            April 2022
+          </Timeline.Time>
+          <Timeline.Title>
+            Titulo 3
+          </Timeline.Title>
+          <Timeline.Body>
+            <p>
+              {placeholder}
+            </p>
+          </Timeline.Body>
+        </Timeline.Content>
+      </Timeline.Item>
+    </Timeline>
+  )
+}
+
+export {TimelineEvents}
--- src/pages/Events/index.jsx
+++ src/pages/Events/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import { TimelineEvents } from '../../components/TimelineEvents'
 
 function Events() {
   return (
     <Layout>
-      Events
+      <h1 className='text-5xl font-[Agbalumo] mt-5'>Eventos</h1>
+      <TimelineEvents />
     </Layout>
   )
 }
